30a6d89a1380220a84fe4fe85e77eba25c39e454
gr1m0ire <gr1m0ire@gr1m0ire.com>
2021-08-28 13:54:20 -0500

    Adjusted startup and configuration files

--- Blockland/serverstart.sh
+++ Blockland/serverstart.sh
@@ -3,7 +3,7 @@
 FILE=/home/container/Blockland.exe
 
 if [ -f "$FILE"  ]; then
-	WINEDEBUG=-all xvfb-run -a wine Blockland.exe ptlaaxobimwroe -dedicated -port ${SERVER_PORT} -gamemode ${GAMEMODE} -dToken ${DTOKEN} -serverName "${SERVER_NAME}"
+	WINEDEBUG=-all xvfb-run -a wine Blockland.exe ptlaaxobimwroe -dedicated -port ${SERVER_PORT} -gamemode ${GAMEMODE} -dToken ${DTOKEN}
 else
-	   WINEDEBUG=-all xvfb-run -a wine Launcher/BlocklandLauncher.exe -profilePath .. -dedicated -port ${SERVER_PORT} -gamemode ${GAMEMODE} -dToken ${DTOKEN} -serverName "${SERVER_NAME}"
+	WINEDEBUG=-all xvfb-run -a wine Launcher/BlocklandLauncher.exe -profilePath .. -dedicated -port ${SERVER_PORT} -gamemode ${GAMEMODE} -dToken ${DTOKEN}
 fi
