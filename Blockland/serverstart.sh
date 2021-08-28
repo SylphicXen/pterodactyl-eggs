39174e167afd6de63098fa04013ae9b88204958c
gr1m0ire <gr1m0ire@gr1m0ire.com>
2021-08-28 13:01:41 -0500

    Added Blockland egg and install script

---
+++ Blockland/serverstart.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+FILE=/home/container/Blockland.exe
+
+if [ -f "$FILE"  ]; then
+	WINEDEBUG=-all xvfb-run -a wine Blockland.exe ptlaaxobimwroe -dedicated -port ${SERVER_PORT} -gamemode ${GAMEMODE} -dToken ${DTOKEN} -serverName "${SERVER_NAME}"
+else
+	   WINEDEBUG=-all xvfb-run -a wine Launcher/BlocklandLauncher.exe -profilePath .. -dedicated -port ${SERVER_PORT} -gamemode ${GAMEMODE} -dToken ${DTOKEN} -serverName "${SERVER_NAME}"
+fi
